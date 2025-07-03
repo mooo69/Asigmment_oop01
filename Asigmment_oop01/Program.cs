14cad742e5295aea8334ec9daeb1865d45d66e0d
mohamed gamal <mohamed.gamalabuy@gmail.com>
2025-07-03 23:29:43 +0300

    Q2

--- Asigmment_oop01/Program.cs
+++ Asigmment_oop01/Program.cs
@@ -14,26 +14,53 @@ namespace Asigmment_oop01
         Sunday
     }
     //=================================================
-
-    class Program
+    //Q2
+    struct Person
     {
-       
-        static void Main(string[] args)
+        public string Name;
+        public int Age;
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+     //===========================================================
+        class Program
         {
 
-
-
-            #region Q1
-            /*
-            //Create an enum called "WeekDays" with the days of the week (Monday to Sunday) as its members. Then, write a C# program that prints out all the days of the week using this enum.
-
-            Console.WriteLine("Days of the week:");
-            foreach (WeekDays day in Enum.GetValues(typeof(WeekDays)))
+            static void Main(string[] args)
             {
-                Console.WriteLine("- " + day);
-            }*/
-            #endregion
 
+
+
+                #region Q1
+                /*
+                //Create an enum called "WeekDays" with the days of the week (Monday to Sunday) as its members. Then, write a C# program that prints out all the days of the week using this enum.
+
+                Console.WriteLine("Days of the week:");
+                foreach (WeekDays day in Enum.GetValues(typeof(WeekDays)))
+                {
+                    Console.WriteLine("- " + day);
+                }*/
+                #endregion
+
+                #region Q2
+                Person[] people = new Person[3];
+
+                people[0] = new Person("Ahmed", 25);
+                people[1] = new Person("Sara", 30);
+                people[2] = new Person("Omar", 22);
+
+                Console.WriteLine("People Information:");
+                for (int i = 0; i < people.Length; i++)
+                {
+                    Console.WriteLine($"Name: {people[i].Name}, Age: {people[i].Age}");
+                }
+                #endregion
+
+
+            }
         }
     }
 }
